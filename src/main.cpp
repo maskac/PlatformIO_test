9f4e9c12bd6df658115494873f4cdc2bbf28b44b
maskac <maska@cgcs.cz>
2016-11-23 00:34:24 +0100

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include  "arduino.h"
+
+void setup (void)
+{
+    pinMode(4,OUTPUT);
+    Serial.begin(9600);
+    Serial.print("Program INIT.....");
+    delay(500);
+    Serial.println("OK");
+}
+
+
+void loop (void)
+{
+    int i;
+    for(i=1;i<=100;i++){
+      Serial.print("Blink no....");
+      Serial.print(i);
+      Serial.print(" ON");
+      digitalWrite(4,HIGH);
+      delay(35);
+      Serial.println(" OFF");
+      digitalWrite(4,LOW);
+      delay(1000);
+    }
+
+    delay(100);
+
+}
